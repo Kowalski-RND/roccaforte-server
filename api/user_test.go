e5fd62da850abb09b699007b2056ba28bf6d324c
Brandon Kowalski <brandon@kowalski.io>
2017-02-03 01:31:39 -0500

    First test with postrgres schema in travis ci

---
+++ api/user_test.go
@@ -0,0 +1,60 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/roccaforte/server/model"
+	"github.com/satori/go.uuid"
+	"net/http"
+	"testing"
+)
+
+var (
+	defaultUUID = uuid.UUID{}
+)
+
+type testUser struct {
+	Fullname  string `db:"fullname" json:"fullname"`
+	Username  string `db:"username" json:"username"`
+	Password  string `db:"password" json:"password,omitempty"`
+	PublicKey string `db:"public_key" json:"public_key,omitempty"`
+}
+
+func TestUserCreation(t *testing.T) {
+
+	u := testUser{
+		Fullname:  "Brandon Kowalski",
+		Username:  "BrandonKowalski",
+		Password:  "ILovePotatoSalad",
+		PublicKey: "This is really fake",
+	}
+
+	en, err := json.Marshal(u)
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	resp, err := http.Post("http://localhost:9090/users", "application/json", bytes.NewBuffer(en))
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	defer resp.Body.Close()
+
+	d := json.NewDecoder(resp.Body)
+
+	c := model.User{}
+
+	err = d.Decode(&c)
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if c.ID == defaultUUID {
+		t.Error("User not saved as default UUID present on returned data")
+	}
+
+}
---
+++ schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY NOT NULL,
+    username TEXT NOT NULL,
+    fullname TEXT NOT NULL,
+    password TEXT NOT NULL,
+    public_key TEXT NOT NULL
+);
+CREATE UNIQUE INDEX user_username_key ON users (username);
+
+CREATE TABLE secrets (
+    id UUID PRIMARY KEY NOT NULL,
+    author UUID NOT NULL,
+    cipher_text TEXT NOT NULL,
+    iv TEXT NOT NULL,
+    CONSTRAINT secret_author_fkey FOREIGN KEY (author) REFERENCES users (id)
+);
+
+CREATE TABLE keys (
+    id UUID PRIMARY KEY NOT NULL,
+    owner UUID NOT NULL,
+    secret UUID NOT NULL,
+    key TEXT NOT NULL,
+    CONSTRAINT key_owner_fkey FOREIGN KEY (owner) REFERENCES users (id),
+    CONSTRAINT key_secret_fkey FOREIGN KEY (secret) REFERENCES secrets (id)
+);
